ae5d6f8a7a135d4d7f24b9b1e441a52e494e8fcf
Kieran <kieranc.inbox@gmail.com>
2022-05-26 01:38:51 +1000

    Copied over and commented on "Modular Behaviour"

--- Assets/MCC/Runtime/Core/ModularBehaviour.cs
+++ Assets/MCC/Runtime/Core/ModularBehaviour.cs
@@ -8,6 +8,9 @@ namespace MCC
 	/// </summary>
 	public abstract class ModularBehaviour : MonoBehaviour
 	{
+		// Creating a public Property "Enabled"
+		// This will enable or disable the state of this component.
+		// When set it forces the class to run OnEnabledStateChanged();
 		public bool Enabled
 		{
 			get => intEnabled;
@@ -18,12 +21,21 @@ namespace MCC
 			}
 		}
 
+		// This is for if the variable is "localOnly" (for multiplayer)
 		public bool LocalOnly => localOnly;
-		
 		[SerializeField] private bool localOnly = true;
+		
+		// This will be how often Process() will be called, defaulting to "Update()"
 		[SerializeField] private UpdatePhase updatePhase = UpdatePhase.Update;
+		
+		// If this component was enabled.
 		private bool intEnabled = true;
-
+		
+		/// <summary>
+		/// Initialisation of this component, passing in the player.
+		/// This is called in the MCCPlayer Start().
+		/// </summary>
+		/// <param name="_player">The Start() of the MCC player this commponent is attached to.</param>
 		public virtual void Init(MCCPlayer _player) { }
 
 		/// <summary>
@@ -39,8 +51,16 @@ namespace MCC
 			OnProcess();
 		}
 
+		/// <summary>
+		/// "Abstract" forces inherited classes to overwrite OnProcess().
+		/// </summary>
 		protected abstract void OnProcess();
 
+		/// <summary>
+		/// This is called when this component is Enabled.
+		/// This allows the derived class to overwrite what happens when this component is enabled.
+		/// </summary>
+		/// <param name="_newState"> If the component is enabled or disabled. </param>
 		protected virtual void OnEnabledStateChanged(bool _newState) { }
 	}
 }
